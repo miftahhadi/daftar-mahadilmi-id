6069a47e373073df3e15e6386488c481a46f80d8
Miftah Hadi S <miftahhadi@gmail.com>
2020-06-22 13:22:32 +0700

    Tambah otorisasi untuk tampilan peserta berdasar jenis kelamin, belum diterapkan

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
+        Gate::define('peserta-ikhwan', function($user){
+           return $user->isAdminIkhwan() ? Response::allow() : $user->isSuperAdmin ? Response::allow() : Response::deny('Anda tidak diizinkan');
+        });
+        
+        Gate::define('peserta-akhawat', function($user){
+           return $user->isAdminAkhawat() ? Response::allow() : $user->isSuperAdmin ? Response::allow() : Response::deny('Anda tidak diizinkan');
+        });
 
         //
     }
